3ee753391d905de57e31463b0be847d92e1f538e
tckz916 <tachikaze0916@gmail.com>
2020-07-17 00:01:32 +0900

    Update Paper Spigot #92

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ rm -f $PWD/plugins/fastasynworldedit.jar
 rm -f $PWD/plugins/fastasyncvoxelsniper.jar
 rm -f $PWD/plugins/multiverse-core.jar
 rm -f $PWD/plugins/cleanroomgenerator.jar
-curl -o $PWD/paper.jar https://papermc.io/api/v1/paper/1.16.1/89/download
+curl -o $PWD/paper.jar https://papermc.io/api/v1/paper/1.16.1/92/download
 curl -o $PWD/plugins/fastasynworldedit.jar https://ci.athion.net/job/FastAsyncWorldEdit-1.16/lastSuccessfulBuild/artifact/worldedit-bukkit/build/libs/FastAsyncWorldEdit-1.16-297.jar
 curl -o $PWD/plugins/fastasyncvoxelsniper.jar https://ci.athion.net/job/FastAsyncVoxelSniper-flattening/lastSuccessfulBuild/artifact/build/libs/FastAsyncVoxelSniper-flattened-1.0.3-backward.jar 
 curl -o $PWD/plugins/multiverse-core.jar https://media.forgecdn.net/files/2768/599/Multiverse-Core-4.1.0.jar 
